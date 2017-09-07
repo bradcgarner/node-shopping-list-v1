a92eb2699165ad082453246a4551382003a0af6b
bradcgarner <brad@bradgarner.com>
2017-09-07 11:30:12 -0400

    get done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+'use strict';
+/*$ global*/
 
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +20,17 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('chocolate milk',['cocoa', 'milk', 'sugar']);
+Recipes.create('chili',['beef', 'beans', 'pepper', 'tomatoes', 'cumin']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
